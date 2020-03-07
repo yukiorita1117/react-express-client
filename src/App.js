7a69a6e67a89d2b47f584a8dcc8d9bcdfd8a88b5
折田幸夫 <3204704@gmail.com>
2020-03-07 20:43:53 +0900

    feat get cotoha api

--- src/App.js
+++ src/App.js
@@ -2,28 +2,37 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  state = { users: [] };
+  state = { users: [], cotoha: [] };
 
   componentDidMount() {
     fetch("/users")
       .then(res => res.json())
       .then(users => this.setState({ users }));
+    fetch("/cotoha")
+      .then(res => res.json())
+      .then(cotoha => this.setState({ cotoha }));
   }
 
   render() {
     return (
-      <div classname="App">
-        <h1>Users</h1>
+      <div className="App">
+        <h1>Usersからdata取得</h1>
         {this.state.users.map(user => (
-          <div key="{user.id}">{user.username}</div>
+          <div key={user.id}>{user.username}</div>
         ))}
-        <h2>POSTING TEST</h2>
-        <form method="post" action="/create">
+
+        <h2>感情分析</h2>
+        <form method="post" action="/cotoha">
           <input type="text" name="name" />
           <br />
           <br />
           <input type="submit" value="createって話" />
         </form>
+
+        <h2>Cotohaからdata取得</h2>
+        {this.state.cotoha.map(cotoha => (
+          <div key={cotoha.id}>{cotoha.text}</div>
+        ))}
       </div>
     );
   }
